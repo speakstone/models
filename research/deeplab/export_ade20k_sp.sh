b9a039d5fe329b65a6bdd6080d7b414a281eecbf
speakstone <1549640983@qq.com>
2018-05-29 11:20:47 +0800

    Create export_ade20k_sp.sh

---
+++ research/deeplab/export_ade20k_sp.sh
@@ -0,0 +1,47 @@
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Move one-level up to tensorflow/models/research directory.
+cd ..
+
+# Set up the working environment.
+CURRENT_DIR=$(pwd)
+WORK_DIR="${CURRENT_DIR}/deeplab"
+SIZE=250
+
+cd "/opt/data1/tensorflow_model/models/research/"
+# Update PYTHONPATH.
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+
+cd "${CURRENT_DIR}"
+
+# Set up the working directories.
+DATASET_DIR="datasets"
+ADE_FOLDER="ADE20K_SP/Supervisely"
+EXP_FOLDER="exp_250/train_on_train_set"
+INIT_FOLDER="${WORK_DIR}/models"
+TRAIN_LOGDIR="${WORK_DIR}/${DATASET_DIR}/${ADE_FOLDER}/${EXP_FOLDER}/train"
+EVAL_LOGDIR="${WORK_DIR}/${DATASET_DIR}/${ADE_FOLDER}/${EXP_FOLDER}/eval"
+VIS_LOGDIR="${WORK_DIR}/${DATASET_DIR}/${ADE_FOLDER}/${EXP_FOLDER}/vis"
+EXPORT_DIR="${WORK_DIR}/${DATASET_DIR}/${ADE_FOLDER}/${EXP_FOLDER}/export"
+mkdir -p "${TRAIN_LOGDIR}"
+mkdir -p "${EVAL_LOGDIR}"
+mkdir -p "${VIS_LOGDIR}"
+mkdir -p "${EXPORT_DIR}"
+
+NUM_ITERATIONS=30000
+  # Export the trained checkpoint.
+CKPT_PATH="${TRAIN_LOGDIR}/model.ckpt-${NUM_ITERATIONS}"
+EXPORT_PATH="${EXPORT_DIR}/frozen_inference_graph.pb"
+
+python "${WORK_DIR}"/export_model.py \
+  --logtostderr \
+  --checkpoint_path="${CKPT_PATH}" \
+  --export_path="${EXPORT_PATH}" \
+  --model_variant="mobilenet_v2" \
+  --output_stride=16 \
+  --decoder_output_stride=4 \
+  --num_classes=3 \
+  --crop_size=${SIZE} \
+  --crop_size=${SIZE} \
+  --inference_scales=1.0
